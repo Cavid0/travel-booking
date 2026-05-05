b5828a5ce453dcb808d7bb76f2923bb233f554f7
Cavid0 <cavidwukurov917@gmail.com>
2026-05-05 16:43:28 +0400

    Improve gateway UI and Render routing

--- car-rental-service/src/main/java/com/takecare/travel/carrentalservice/CarRentalServiceApplication.java
+++ car-rental-service/src/main/java/com/takecare/travel/carrentalservice/CarRentalServiceApplication.java
@@ -1,7 +1,8 @@
 package com.takecare.travel.carrentalservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class CarRentalServiceApplication { public static void main(String[] args) { SpringApplication.run(CarRentalServiceApplication.class, args); } }
